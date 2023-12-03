03257ae4aeb02a2cc361aeb27fa092b426c44790
ZhdanovaAnna <annaone01@mail.ru>
2023-12-03 17:50:51 +0500

    Task0

--- Tyuiu.ZhdanovaAA.Sprint6.Task0.V29/FormMain.cs
+++ Tyuiu.ZhdanovaAA.Sprint6.Task0.V29/FormMain.cs
@@ -32,17 +32,17 @@ namespace Tyuiu.ZhdanovaAA.Sprint6.Task0.V29
             }
         }
 
-        private void buttonHelp_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Таск 0 выполнил студент группы ПКТб-23-1 Жданова Анна Александровна", "Сообщение"); 
-        }
-
         private void textBoxVarX_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != ',') && (e.KeyChar != 8))
             {
                 e.Handled = true;
             }
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 0 выполнил студент группы ПКТб-23-1 Жданова Анна Александровна", "Сообщение"); 
         }
     }
 }
