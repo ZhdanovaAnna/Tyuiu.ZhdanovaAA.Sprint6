05607bff3d4642c4564d83315da2d799dea49ac7
ZhdanovaAnna <annaone01@mail.ru>
2023-12-10 18:00:33 +0500

    Task4

--- Tyuiu.ZhdanovaAA.Sprint6.Task4.V30/FormMain.Designer.cs
+++ Tyuiu.ZhdanovaAA.Sprint6.Task4.V30/FormMain.Designer.cs
@@ -29,10 +29,10 @@ namespace Tyuiu.ZhdanovaAA.Sprint6.Task4.V30
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.panelCondition_ZAA = new System.Windows.Forms.Panel();
             this.buttonHelp_ZAA = new System.Windows.Forms.Button();
             this.buttonSave_ZAA = new System.Windows.Forms.Button();
@@ -249,31 +249,29 @@ namespace Tyuiu.ZhdanovaAA.Sprint6.Task4.V30
             // 
             // chartFunction_ZAA
             // 
-            this.chartFunction_ZAA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea3.Name = "ChartArea1";
-            this.chartFunction_ZAA.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chartFunction_ZAA.Legends.Add(legend3);
-            this.chartFunction_ZAA.Location = new System.Drawing.Point(0, 4);
+            chartArea1.Name = "ChartArea1";
+            this.chartFunction_ZAA.ChartAreas.Add(chartArea1);
+            this.chartFunction_ZAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chartFunction_ZAA.Legends.Add(legend1);
+            this.chartFunction_ZAA.Location = new System.Drawing.Point(0, 0);
             this.chartFunction_ZAA.Margin = new System.Windows.Forms.Padding(2);
             this.chartFunction_ZAA.Name = "chartFunction_ZAA";
             this.chartFunction_ZAA.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            series3.IsVisibleInLegend = false;
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chartFunction_ZAA.Series.Add(series3);
-            this.chartFunction_ZAA.Size = new System.Drawing.Size(739, 366);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartFunction_ZAA.Series.Add(series1);
+            this.chartFunction_ZAA.Size = new System.Drawing.Size(741, 370);
             this.chartFunction_ZAA.TabIndex = 0;
-            title3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            title3.ForeColor = System.Drawing.SystemColors.HotTrack;
-            title3.Name = "TitleFunction_ZAA";
-            title3.Text = "График функции F(x)";
-            this.chartFunction_ZAA.Titles.Add(title3);
+            title1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            title1.Name = "TitleFunction_ZAA";
+            title1.Text = "График функции F(x)";
+            this.chartFunction_ZAA.Titles.Add(title1);
             // 
             // splitterOutputData_ZAA
             // 
