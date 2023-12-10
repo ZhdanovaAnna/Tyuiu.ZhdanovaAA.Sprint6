d4f5ccd8a360a0126c5ca79431e5dbef2a3f718d
ZhdanovaAnna <annaone01@mail.ru>
2023-12-11 01:31:50 +0500

    Task6

--- Tyuiu.ZhdanovaAA.Sprint6.Task6.V11/FormMain.Designer.cs
+++ Tyuiu.ZhdanovaAA.Sprint6.Task6.V11/FormMain.Designer.cs
@@ -31,147 +31,44 @@ namespace Tyuiu.ZhdanovaAA.Sprint6.Task6.V11
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panelCondition_ZAA = new System.Windows.Forms.Panel();
+            this.panelHelp_ZAA = new System.Windows.Forms.Panel();
+            this.buttonHelp_ZAA = new System.Windows.Forms.Button();
+            this.buttonDone_ZAA = new System.Windows.Forms.Button();
+            this.buttonLoad_ZAA = new System.Windows.Forms.Button();
+            this.panelInputData_ZAA = new System.Windows.Forms.Panel();
             this.groupBoxInputData_ZAA = new System.Windows.Forms.GroupBox();
             this.textBoxInputData_ZAA = new System.Windows.Forms.TextBox();
             this.panelConditon_ZAA = new System.Windows.Forms.Panel();
             this.groupBoxCondition_ZAA = new System.Windows.Forms.GroupBox();
             this.labelCondition_ZAA = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.panelOutputData_ZAA = new System.Windows.Forms.Panel();
             this.groupBoxOutputData_ZAA = new System.Windows.Forms.GroupBox();
             this.textBoxOutputData_ZAA = new System.Windows.Forms.TextBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.openFileDialogTask_ZAA = new System.Windows.Forms.OpenFileDialog();
             this.toolTipTask_ZAA = new System.Windows.Forms.ToolTip(this.components);
-            this.buttonHelp_ZAA = new System.Windows.Forms.Button();
-            this.buttonDone_ZAA = new System.Windows.Forms.Button();
-            this.buttonLoad_ZAA = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panelCondition_ZAA.SuspendLayout();
+            this.panelHelp_ZAA.SuspendLayout();
             this.groupBoxInputData_ZAA.SuspendLayout();
             this.panelConditon_ZAA.SuspendLayout();
             this.groupBoxCondition_ZAA.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.panelOutputData_ZAA.SuspendLayout();
             this.groupBoxOutputData_ZAA.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelHelp_ZAA
             // 
-            this.panel1.Controls.Add(this.buttonHelp_ZAA);
-            this.panel1.Controls.Add(this.buttonDone_ZAA);
-            this.panel1.Controls.Add(this.buttonLoad_ZAA);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1012, 71);
-            this.panel1.TabIndex = 0;
-            // 
-            // panelCondition_ZAA
-            // 
-            this.panelCondition_ZAA.Controls.Add(this.groupBoxInputData_ZAA);
-            this.panelCondition_ZAA.Location = new System.Drawing.Point(0, 181);
-            this.panelCondition_ZAA.Name = "panelCondition_ZAA";
-            this.panelCondition_ZAA.Size = new System.Drawing.Size(499, 356);
-            this.panelCondition_ZAA.TabIndex = 0;
-            // 
-            // groupBoxInputData_ZAA
-            // 
-            this.groupBoxInputData_ZAA.Controls.Add(this.textBoxInputData_ZAA);
-            this.groupBoxInputData_ZAA.Location = new System.Drawing.Point(13, 4);
-            this.groupBoxInputData_ZAA.Name = "groupBoxInputData_ZAA";
-            this.groupBoxInputData_ZAA.Size = new System.Drawing.Size(483, 348);
-            this.groupBoxInputData_ZAA.TabIndex = 0;
-            this.groupBoxInputData_ZAA.TabStop = false;
-            this.groupBoxInputData_ZAA.Text = "Ввод:";
-            // 
-            // textBoxInputData_ZAA
-            // 
-            this.textBoxInputData_ZAA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxInputData_ZAA.Location = new System.Drawing.Point(3, 18);
-            this.textBoxInputData_ZAA.Multiline = true;
-            this.textBoxInputData_ZAA.Name = "textBoxInputData_ZAA";
-            this.textBoxInputData_ZAA.ReadOnly = true;
-            this.textBoxInputData_ZAA.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxInputData_ZAA.Size = new System.Drawing.Size(477, 327);
-            this.textBoxInputData_ZAA.TabIndex = 0;
-            // 
-            // panelConditon_ZAA
-            // 
-            this.panelConditon_ZAA.Controls.Add(this.groupBoxCondition_ZAA);
-            this.panelConditon_ZAA.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelConditon_ZAA.Location = new System.Drawing.Point(0, 71);
-            this.panelConditon_ZAA.Name = "panelConditon_ZAA";
-            this.panelConditon_ZAA.Size = new System.Drawing.Size(1012, 104);
-            this.panelConditon_ZAA.TabIndex = 0;
-            // 
-            // groupBoxCondition_ZAA
-            // 
-            this.groupBoxCondition_ZAA.Controls.Add(this.labelCondition_ZAA);
-            this.groupBoxCondition_ZAA.Location = new System.Drawing.Point(13, 7);
-            this.groupBoxCondition_ZAA.Name = "groupBoxCondition_ZAA";
-            this.groupBoxCondition_ZAA.Size = new System.Drawing.Size(987, 94);
-            this.groupBoxCondition_ZAA.TabIndex = 0;
-            this.groupBoxCondition_ZAA.TabStop = false;
-            this.groupBoxCondition_ZAA.Text = "Условие";
-            // 
-            // labelCondition_ZAA
-            // 
-            this.labelCondition_ZAA.AutoSize = true;
-            this.labelCondition_ZAA.Location = new System.Drawing.Point(7, 22);
-            this.labelCondition_ZAA.Name = "labelCondition_ZAA";
-            this.labelCondition_ZAA.Size = new System.Drawing.Size(887, 34);
-            this.labelCondition_ZAA.TabIndex = 0;
-            this.labelCondition_ZAA.Text = resources.GetString("labelCondition_ZAA.Text");
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.groupBoxOutputData_ZAA);
-            this.panel4.Location = new System.Drawing.Point(505, 181);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(504, 356);
-            this.panel4.TabIndex = 0;
-            // 
-            // groupBoxOutputData_ZAA
-            // 
-            this.groupBoxOutputData_ZAA.Controls.Add(this.textBoxOutputData_ZAA);
-            this.groupBoxOutputData_ZAA.Location = new System.Drawing.Point(4, 4);
-            this.groupBoxOutputData_ZAA.Name = "groupBoxOutputData_ZAA";
-            this.groupBoxOutputData_ZAA.Size = new System.Drawing.Size(491, 348);
-            this.groupBoxOutputData_ZAA.TabIndex = 0;
-            this.groupBoxOutputData_ZAA.TabStop = false;
-            this.groupBoxOutputData_ZAA.Text = "Вывод:";
-            // 
-            // textBoxOutputData_ZAA
-            // 
-            this.textBoxOutputData_ZAA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxOutputData_ZAA.Location = new System.Drawing.Point(3, 18);
-            this.textBoxOutputData_ZAA.Multiline = true;
-            this.textBoxOutputData_ZAA.Name = "textBoxOutputData_ZAA";
-            this.textBoxOutputData_ZAA.ReadOnly = true;
-            this.textBoxOutputData_ZAA.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxOutputData_ZAA.Size = new System.Drawing.Size(485, 327);
-            this.textBoxOutputData_ZAA.TabIndex = 0;
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(0, 175);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 370);
-            this.splitter1.TabIndex = 1;
-            this.splitter1.TabStop = false;
-            // 
-            // openFileDialogTask_ZAA
-            // 
-            this.openFileDialogTask_ZAA.FileName = "openFileDialog1";
-            // 
-            // toolTipTask_ZAA
-            // 
-            this.toolTipTask_ZAA.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.toolTipTask_ZAA.ToolTipTitle = "Подсказка";
+            this.panelHelp_ZAA.Controls.Add(this.buttonHelp_ZAA);
+            this.panelHelp_ZAA.Controls.Add(this.buttonDone_ZAA);
+            this.panelHelp_ZAA.Controls.Add(this.buttonLoad_ZAA);
+            this.panelHelp_ZAA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHelp_ZAA.Location = new System.Drawing.Point(0, 0);
+            this.panelHelp_ZAA.Name = "panelHelp_ZAA";
+            this.panelHelp_ZAA.Size = new System.Drawing.Size(1012, 71);
+            this.panelHelp_ZAA.TabIndex = 0;
             // 
             // buttonHelp_ZAA
             // 
+            this.buttonHelp_ZAA.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonHelp_ZAA.BackColor = System.Drawing.Color.Transparent;
             this.buttonHelp_ZAA.BackgroundImage = global::Tyuiu.ZhdanovaAA.Sprint6.Task6.V11.Properties.Resources.help;
             this.buttonHelp_ZAA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
@@ -210,28 +107,141 @@ namespace Tyuiu.ZhdanovaAA.Sprint6.Task6.V11
             this.buttonLoad_ZAA.UseVisualStyleBackColor = false;
             this.buttonLoad_ZAA.Click += new System.EventHandler(this.buttonLoad_ZAA_Click);
             // 
+            // panelInputData_ZAA
+            // 
+            this.panelInputData_ZAA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panelInputData_ZAA.Location = new System.Drawing.Point(0, 181);
+            this.panelInputData_ZAA.Name = "panelInputData_ZAA";
+            this.panelInputData_ZAA.Size = new System.Drawing.Size(499, 364);
+            this.panelInputData_ZAA.TabIndex = 0;
+            // 
+            // groupBoxInputData_ZAA
+            // 
+            this.groupBoxInputData_ZAA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBoxInputData_ZAA.Controls.Add(this.textBoxInputData_ZAA);
+            this.groupBoxInputData_ZAA.Location = new System.Drawing.Point(13, 178);
+            this.groupBoxInputData_ZAA.Name = "groupBoxInputData_ZAA";
+            this.groupBoxInputData_ZAA.Size = new System.Drawing.Size(483, 363);
+            this.groupBoxInputData_ZAA.TabIndex = 0;
+            this.groupBoxInputData_ZAA.TabStop = false;
+            this.groupBoxInputData_ZAA.Text = "Ввод:";
+            // 
+            // textBoxInputData_ZAA
+            // 
+            this.textBoxInputData_ZAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInputData_ZAA.Location = new System.Drawing.Point(3, 18);
+            this.textBoxInputData_ZAA.Multiline = true;
+            this.textBoxInputData_ZAA.Name = "textBoxInputData_ZAA";
+            this.textBoxInputData_ZAA.ReadOnly = true;
+            this.textBoxInputData_ZAA.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInputData_ZAA.Size = new System.Drawing.Size(477, 342);
+            this.textBoxInputData_ZAA.TabIndex = 0;
+            // 
+            // panelConditon_ZAA
+            // 
+            this.panelConditon_ZAA.Controls.Add(this.groupBoxCondition_ZAA);
+            this.panelConditon_ZAA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelConditon_ZAA.Location = new System.Drawing.Point(0, 71);
+            this.panelConditon_ZAA.Name = "panelConditon_ZAA";
+            this.panelConditon_ZAA.Size = new System.Drawing.Size(1012, 104);
+            this.panelConditon_ZAA.TabIndex = 0;
+            // 
+            // groupBoxCondition_ZAA
+            // 
+            this.groupBoxCondition_ZAA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxCondition_ZAA.Controls.Add(this.labelCondition_ZAA);
+            this.groupBoxCondition_ZAA.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxCondition_ZAA.Location = new System.Drawing.Point(13, 7);
+            this.groupBoxCondition_ZAA.Name = "groupBoxCondition_ZAA";
+            this.groupBoxCondition_ZAA.Size = new System.Drawing.Size(987, 94);
+            this.groupBoxCondition_ZAA.TabIndex = 0;
+            this.groupBoxCondition_ZAA.TabStop = false;
+            this.groupBoxCondition_ZAA.Text = "Условие";
+            // 
+            // labelCondition_ZAA
+            // 
+            this.labelCondition_ZAA.AutoSize = true;
+            this.labelCondition_ZAA.Location = new System.Drawing.Point(7, 22);
+            this.labelCondition_ZAA.Name = "labelCondition_ZAA";
+            this.labelCondition_ZAA.Size = new System.Drawing.Size(760, 54);
+            this.labelCondition_ZAA.TabIndex = 0;
+            this.labelCondition_ZAA.Text = resources.GetString("labelCondition_ZAA.Text");
+            // 
+            // panelOutputData_ZAA
+            // 
+            this.panelOutputData_ZAA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelOutputData_ZAA.Controls.Add(this.groupBoxOutputData_ZAA);
+            this.panelOutputData_ZAA.Location = new System.Drawing.Point(505, 181);
+            this.panelOutputData_ZAA.Name = "panelOutputData_ZAA";
+            this.panelOutputData_ZAA.Size = new System.Drawing.Size(504, 356);
+            this.panelOutputData_ZAA.TabIndex = 0;
+            // 
+            // groupBoxOutputData_ZAA
+            // 
+            this.groupBoxOutputData_ZAA.Controls.Add(this.textBoxOutputData_ZAA);
+            this.groupBoxOutputData_ZAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxOutputData_ZAA.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxOutputData_ZAA.Name = "groupBoxOutputData_ZAA";
+            this.groupBoxOutputData_ZAA.Size = new System.Drawing.Size(504, 356);
+            this.groupBoxOutputData_ZAA.TabIndex = 0;
+            this.groupBoxOutputData_ZAA.TabStop = false;
+            this.groupBoxOutputData_ZAA.Text = "Вывод:";
+            // 
+            // textBoxOutputData_ZAA
+            // 
+            this.textBoxOutputData_ZAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOutputData_ZAA.Location = new System.Drawing.Point(3, 18);
+            this.textBoxOutputData_ZAA.Multiline = true;
+            this.textBoxOutputData_ZAA.Name = "textBoxOutputData_ZAA";
+            this.textBoxOutputData_ZAA.ReadOnly = true;
+            this.textBoxOutputData_ZAA.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutputData_ZAA.Size = new System.Drawing.Size(498, 335);
+            this.textBoxOutputData_ZAA.TabIndex = 0;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 175);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 370);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // openFileDialogTask_ZAA
+            // 
+            this.openFileDialogTask_ZAA.FileName = "openFileDialog1";
+            // 
+            // toolTipTask_ZAA
+            // 
+            this.toolTipTask_ZAA.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTipTask_ZAA.ToolTipTitle = "Подсказка";
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1012, 545);
+            this.Controls.Add(this.groupBoxInputData_ZAA);
             this.Controls.Add(this.splitter1);
-            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panelOutputData_ZAA);
             this.Controls.Add(this.panelConditon_ZAA);
-            this.Controls.Add(this.panelCondition_ZAA);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelInputData_ZAA);
+            this.Controls.Add(this.panelHelp_ZAA);
             this.MinimumSize = new System.Drawing.Size(1030, 592);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Спринт 6 | Таск 6 | Вариант 11 | Жданова А. А.";
-            this.panel1.ResumeLayout(false);
-            this.panelCondition_ZAA.ResumeLayout(false);
+            this.panelHelp_ZAA.ResumeLayout(false);
             this.groupBoxInputData_ZAA.ResumeLayout(false);
             this.groupBoxInputData_ZAA.PerformLayout();
             this.panelConditon_ZAA.ResumeLayout(false);
             this.groupBoxCondition_ZAA.ResumeLayout(false);
             this.groupBoxCondition_ZAA.PerformLayout();
-            this.panel4.ResumeLayout(false);
+            this.panelOutputData_ZAA.ResumeLayout(false);
             this.groupBoxOutputData_ZAA.ResumeLayout(false);
             this.groupBoxOutputData_ZAA.PerformLayout();
             this.ResumeLayout(false);
@@ -240,17 +250,17 @@ namespace Tyuiu.ZhdanovaAA.Sprint6.Task6.V11
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelHelp_ZAA;
         private System.Windows.Forms.Button buttonHelp_ZAA;
         private System.Windows.Forms.Button buttonDone_ZAA;
         private System.Windows.Forms.Button buttonLoad_ZAA;
-        private System.Windows.Forms.Panel panelCondition_ZAA;
+        private System.Windows.Forms.Panel panelInputData_ZAA;
         private System.Windows.Forms.GroupBox groupBoxInputData_ZAA;
         private System.Windows.Forms.TextBox textBoxInputData_ZAA;
         private System.Windows.Forms.Panel panelConditon_ZAA;
         private System.Windows.Forms.GroupBox groupBoxCondition_ZAA;
         private System.Windows.Forms.Label labelCondition_ZAA;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panelOutputData_ZAA;
         private System.Windows.Forms.GroupBox groupBoxOutputData_ZAA;
         private System.Windows.Forms.TextBox textBoxOutputData_ZAA;
         private System.Windows.Forms.Splitter splitter1;
